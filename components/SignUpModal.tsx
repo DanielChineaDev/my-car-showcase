85aaa07df032eb8ae0159eca88008df929806578
Emilio R. Estévez González <eeg@pipelinepro.io>
2024-09-07 02:51:25 +0100

    Added a password strength meter

--- components/SignUpModal.tsx
+++ components/SignUpModal.tsx
@@ -11,6 +11,7 @@ const SignUpModal: React.FC<{ isOpen: boolean; onClose: () => void; onSwitchToLo
   const [emailError, setEmailError] = useState('');
   const [password, setPassword] = useState('');
   const [passwordError, setPasswordError] = useState('');
+  const [passwordStrength, setPasswordStrength] = useState(0); // Medidor de fuerza de contraseña
 
   // Expresión regular para validar correos electrónicos
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -18,17 +19,28 @@ const SignUpModal: React.FC<{ isOpen: boolean; onClose: () => void; onSwitchToLo
   // Expresión regular para validar contraseñas seguras
   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-  // Función para capitalizar la primera letra después de cada espacio
+  // Función para capitalizar la primera letra de cada palabra
   const capitalizeWords = (value: string) => {
     return value.replace(/\b\w/g, (char) => char.toUpperCase());
   };
 
-  // Función para filtrar caracteres especiales y controlar los espacios
+  // Función para filtrar caracteres especiales y manejar espacios
   const filterSpecialCharactersAndSpaces = (value: string) => {
-    let filtered = value.replace(/[^a-zA-Z\s]/g, ''); // Solo permite letras y espacios
-    filtered = filtered.replace(/\s+/g, ' '); // Reemplaza múltiples espacios con uno solo
-    filtered = filtered.trimStart(); // Elimina el espacio inicial
+    let filtered = value.replace(/[^a-zA-Z\s]/g, ''); // Solo letras y espacios
+    filtered = filtered.replace(/\s+/g, ' '); // Reemplaza múltiples espacios con uno
+    filtered = filtered.trimStart(); // Elimina espacios iniciales
     return filtered;
+  };
+
+  // Función para medir la fuerza de la contraseña
+  const calculatePasswordStrength = (password: string) => {
+    let strength = 0;
+    if (password.length >= 8) strength++;
+    if (/[A-Z]/.test(password)) strength++;
+    if (/[a-z]/.test(password)) strength++;
+    if (/\d/.test(password)) strength++;
+    if (/[@$!%*?&]/.test(password)) strength++;
+    return strength; // Devuelve un valor entre 0 y 5
   };
 
   const handleFirstNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -48,7 +60,6 @@ const SignUpModal: React.FC<{ isOpen: boolean; onClose: () => void; onSwitchToLo
     const input = e.target.value;
     setEmail(input);
 
-    // Verificar si el correo es válido
     if (!emailRegex.test(input) && input !== '') {
       setEmailError('Por favor, introduce un email válido');
     } else {
@@ -56,19 +67,20 @@ const SignUpModal: React.FC<{ isOpen: boolean; onClose: () => void; onSwitchToLo
     }
   };
 
-  // Ocultar el error del email al perder el foco si el campo está vacío
+  // Ocultar el error del email al salir del campo si está vacío
   const handleEmailBlur = () => {
     if (email === '') {
       setEmailError('');
     }
   };
 
-  // Validación de la contraseña
+  // Controlador para el cambio de contraseña
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const input = e.target.value;
     setPassword(input);
+    const strength = calculatePasswordStrength(input);
+    setPasswordStrength(strength);
 
-    // Verificar si la contraseña es válida
     if (!passwordRegex.test(input) && input !== '') {
       setPasswordError('La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial.');
     } else {
@@ -76,7 +88,7 @@ const SignUpModal: React.FC<{ isOpen: boolean; onClose: () => void; onSwitchToLo
     }
   };
 
-  // Ocultar el error de la contraseña al perder el foco si el campo está vacío
+  // Ocultar el error de la contraseña si está vacía al perder el foco
   const handlePasswordBlur = () => {
     if (password === '') {
       setPasswordError('');
@@ -87,18 +99,18 @@ const SignUpModal: React.FC<{ isOpen: boolean; onClose: () => void; onSwitchToLo
     setAgree(!agree);
   };
 
-  if (!isOpen) return null; // Si el modal no está abierto, no se renderiza nada
+  if (!isOpen) return null; // Si el modal no está abierto, no renderiza nada
 
   return (
     <div className={styles.modalOverlay} onClick={onClose}>
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
-        {/* Panel izquierdo con el mensaje de la comunidad */}
+        {/* Panel izquierdo con mensaje de la comunidad */}
         <div className={styles.modalLeft}>
           <h3>Únete a la comunidad de coches más grande</h3>
           <p>Forma parte de algo grande.</p>
         </div>
 
-        {/* Panel derecho con el formulario para crear una cuenta */}
+        {/* Panel derecho con el formulario de registro */}
         <div className={styles.modalRight}>
           <h2>¡Únete al club!</h2>
           <p>
@@ -129,9 +141,9 @@ const SignUpModal: React.FC<{ isOpen: boolean; onClose: () => void; onSwitchToLo
               className={`${styles.input} ${styles.fullWidth}`}
               value={email}
               onChange={handleEmailChange}
-              onBlur={handleEmailBlur} // Se activa al salir del campo de email
+              onBlur={handleEmailBlur}
             />
-            {emailError && <p className={styles.errorTextEmail}>{emailError}</p>} {/* Mostrar mensaje de error */}
+            {emailError && <p className={styles.errorTextEmail}>{emailError}</p>}
 
             <input
               type="password"
@@ -139,9 +151,29 @@ const SignUpModal: React.FC<{ isOpen: boolean; onClose: () => void; onSwitchToLo
               className={`${styles.input} ${styles.fullWidth}`}
               value={password}
               onChange={handlePasswordChange}
-              onBlur={handlePasswordBlur} // Se activa al salir del campo de contraseña
+              onBlur={handlePasswordBlur}
             />
-            {passwordError && <p className={styles.errorTextPassword}>{passwordError}</p>} {/* Mostrar mensaje de error */}
+            {passwordError && <p className={styles.errorTextPassword}>{passwordError}</p>}
+
+            {/* Barra de fuerza de la contraseña con animación */}
+            <div className={styles.passwordStrength}>
+              {[...Array(5)].map((_, i) => {
+                const isFilled = i < passwordStrength; // Define si el segmento está lleno o vacío
+                return (
+                  <div
+                    key={i}
+                    className={`${styles.strengthBar} ${isFilled ? styles.filled : ''}`}
+                    style={{
+                      transition: `background-color ${isFilled ? 0.5 : 0.2}s ${isFilled ? i * 0.1 : (4 - i) * 0.05}s ease-in-out`,
+                    }}
+                  />
+                );
+              })}
+              <span>
+                {passwordStrength <= 2 ? 'Poco segura' : passwordStrength <= 4 ? 'Segura' : 'Muy segura'}
+              </span>
+            </div>
+
 
             <div className={styles.checkboxContainer}>
               <input
