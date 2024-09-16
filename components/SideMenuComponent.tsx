c49194ca18e50219809e25f1fe01fdbab127e11a
DatBlueDev <l3lueart@gmail.com>
2024-09-16 14:28:03 +0100

    Fragment logic, SideMenu styled added

--- components/SideMenuComponent.tsx
+++ components/SideMenuComponent.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import { getAuth, signOut } from 'firebase/auth';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome, faWarehouse, faFolderClosed, faCircleXmark, faGear, faGrip } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faWarehouse, faFolderClosed, faGear, faGrip } from "@fortawesome/free-solid-svg-icons";
+import { getAuth, signOut } from 'firebase/auth';
 import styles from '../styles/SideMenuComponent.module.css';
 import Image from 'next/image';
 
-const Sidebar = ({ user }) => {
+const Sidebar = ({ user, selectedSection, onSectionChange }) => {
   const router = useRouter();
 
   const handleLogout = async () => {
     const auth = getAuth();
     try {
-      await signOut(auth); // Cerrar sesión en Firebase
-      router.push('/'); // Redireccionar a la página de inicio
+      await signOut(auth);
+      router.push('/');
     } catch (error) {
       console.error("Error al cerrar sesión:", error);
     }
@@ -24,22 +24,22 @@ const Sidebar = ({ user }) => {
       {/* Verificar si los datos del usuario están disponibles */}
       {user ? (
         <div className={styles.userCard}>
-  <div className={styles.avatarContainer}>
-    <Image
-      src={user.avatarUrl}
-      alt={`${user.firstName} ${user.lastName}`}
-      layout="fill" // Esto permite que la imagen ocupe todo el contenedor
-      objectFit="cover" // Esto asegura que la imagen mantenga la proporción y llene el área del contenedor
-      className={styles.userAvatar}
-    />
-  </div>
-  <div>
-    <p className={styles.userName}>{user.firstName} {user.lastName}</p>
-    <p className={styles.userRole}>
-      {user.role === 'admin' ? 'Administrador' : 'Miembro'}
-    </p>
-  </div>
-</div>
+          <div className={styles.avatarContainer}>
+            <Image
+              src={user.avatarUrl}
+              alt={`${user.firstName} ${user.lastName}`}
+              layout="fill"
+              objectFit="cover"
+              className={styles.userAvatar}
+            />
+          </div>
+          <div>
+            <p className={styles.userName}>{user.firstName} {user.lastName}</p>
+            <p className={styles.userRole}>
+              {user.role === 'admin' ? 'Administrador' : 'Miembro'}
+            </p>
+          </div>
+        </div>
       ) : (
         <p>Cargando usuario...</p>
       )}
@@ -47,16 +47,24 @@ const Sidebar = ({ user }) => {
       {/* Sección Cuenta */}
       <h3 className={styles.sectionTitle}>Cuenta</h3>
       <ul className={styles.menu}>
-        <li className={styles.menuItem}>
+        <li
+          className={`${styles.menuItem} ${selectedSection === 'general' ? styles.active : ''}`}
+          onClick={() => onSectionChange('general')}
+        >
           <FontAwesomeIcon icon={faHome} className={styles.icon} />
           <span className={styles.text}>General</span>
         </li>
-        <li className={styles.menuItem}>
+        <li
+          className={`${styles.menuItem} ${selectedSection === 'garage' ? styles.active : ''}`}
+          onClick={() => onSectionChange('garage')}
+        >
           <FontAwesomeIcon icon={faWarehouse} className={styles.icon} />
           <span className={styles.text}>Garaje</span>
-          <span className={styles.badge}>4</span>
         </li>
-        <li className={styles.menuItem}>
+        <li
+          className={`${styles.menuItem} ${selectedSection === 'settings' ? styles.active : ''}`}
+          onClick={() => onSectionChange('settings')}
+        >
           <FontAwesomeIcon icon={faGear} className={styles.icon} />
           <span className={styles.text}>Configuración</span>
         </li>
@@ -67,17 +75,22 @@ const Sidebar = ({ user }) => {
       {/* Sección Admin */}
       <h3 className={styles.sectionTitle}>Admin</h3>
       <ul className={styles.menu}>
-        <li className={styles.menuItem}>
+        <li
+          className={`${styles.menuItem} ${selectedSection === 'adminPanel' ? styles.active : ''}`}
+          onClick={() => onSectionChange('adminPanel')}
+        >
           <FontAwesomeIcon icon={faGrip} className={styles.icon} />
           <span className={styles.text}>Panel de administrador</span>
         </li>
-        <li className={styles.menuItem}>
+        <li
+          className={`${styles.menuItem} ${selectedSection === 'requests' ? styles.active : ''}`}
+          onClick={() => onSectionChange('requests')}
+        >
           <FontAwesomeIcon icon={faFolderClosed} className={styles.icon} />
           <span className={styles.text}>Solicitudes</span>
         </li>
       </ul>
 
-      {/* Botón de cerrar sesión */}
       <button onClick={handleLogout} className={styles.logoutButton}>
         <span>Cerrar sesión</span>
       </button>
--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,26 +1,17 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import { doc, getDoc, updateDoc } from 'firebase/firestore';
-import { db, storage } from '../../firebaseConfig'; // Asegúrate de tener configuración de Firebase Storage
-import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-import Image from 'next/image'; // Usaremos Next.js Image
-import styles from '../../styles/ProfilePage.module.css'; // Asegúrate de que el archivo CSS esté correctamente referenciado
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../../firebaseConfig';
 import Sidebar from '@/components/SideMenuComponent';
+import styles from '../../styles/ProfilePage.module.css';
 
 const ProfilePage = () => {
   const router = useRouter();
   const { id } = router.query; // El ID del usuario desde la URL
-  const [userData, setUserData] = useState<any>(null); // Estado para los datos del usuario
+  const [selectedSection, setSelectedSection] = useState('general'); // Controla la sección seleccionada
+  const [userData, setUserData] = useState<any>(null);
   const [loading, setLoading] = useState(true);
-  const [editableFields, setEditableFields] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    notifications: 0, // Campo para notificaciones
-    avatarUrl: '/mnt/data/original-4d4042835fd3881071c03326dccbc14d.webp', // Aquí se usará la imagen cargada
-  });
   const [error, setError] = useState('');
-  const [imageFile, setImageFile] = useState<File | null>(null); // Archivo de la imagen de perfil
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -28,17 +19,8 @@ const ProfilePage = () => {
         try {
           const userDocRef = doc(db, 'users', id as string);
           const userDocSnap = await getDoc(userDocRef);
-
           if (userDocSnap.exists()) {
-            const data = userDocSnap.data();
-            setUserData(data);
-            setEditableFields({
-              firstName: data.firstName || '',
-              lastName: data.lastName || '',
-              email: data.email || '',
-              notifications: data.notifications || 0,
-              avatarUrl: data.avatarUrl || '/mnt/data/original-4d4042835fd3881071c03326dccbc14d.webp', // Usando imagen cargada
-            });
+            setUserData(userDocSnap.data());
           } else {
             setError('Usuario no encontrado');
           }
@@ -49,60 +31,12 @@ const ProfilePage = () => {
         }
       }
     };
-
     fetchUserData();
   }, [id]);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setEditableFields({
-      ...editableFields,
-      [name]: value,
-    });
-  };
-
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files[0]) {
-      setImageFile(e.target.files[0]);
-    }
-  };
-
-  const uploadProfileImage = async (file: File) => {
-    const storageRef = ref(storage, `avatars/${id}`);
-    await uploadBytes(storageRef, file);
-    const downloadURL = await getDownloadURL(storageRef);
-    return downloadURL;
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      let newAvatarUrl = editableFields.avatarUrl;
-
-      // Si hay una nueva imagen seleccionada, sube la imagen al almacenamiento de Firebase
-      if (imageFile) {
-        newAvatarUrl = await uploadProfileImage(imageFile);
-      }
-
-      const userDocRef = doc(db, 'users', id as string);
-      await updateDoc(userDocRef, {
-        ...editableFields,
-        avatarUrl: newAvatarUrl, // Actualiza la imagen de perfil
-      });
-      alert('Perfil actualizado correctamente');
-    } catch (err) {
-      setError('Error al actualizar el perfil');
-    }
-  };
-
-  const handleMarkAsRead = async () => {
-    try {
-      const userDocRef = doc(db, 'users', id as string);
-      await updateDoc(userDocRef, { notifications: 0 }); // Marcar notificaciones como leídas
-      setEditableFields((prev) => ({ ...prev, notifications: 0 }));
-    } catch (err) {
-      setError('Error al marcar notificaciones como leídas');
-    }
+  // Controla la sección activa en el menú
+  const handleSectionChange = (section: string) => {
+    setSelectedSection(section);
   };
 
   if (loading) {
@@ -115,74 +49,40 @@ const ProfilePage = () => {
 
   return (
     <div className={styles.container}>
-      <Sidebar user={editableFields}/>
+      <Sidebar user={userData} selectedSection={selectedSection} onSectionChange={handleSectionChange} />
 
       <div className={styles.profileContent}>
-        <h1>Perfil de {editableFields.firstName} {editableFields.lastName}</h1>
-
-        {editableFields.notifications > 0 && (
+        {/* Renderiza contenido basado en la sección seleccionada */}
+        {selectedSection === 'general' && (
           <div>
-            <p>Tienes {editableFields.notifications} notificaciones pendientes</p>
-            <button onClick={handleMarkAsRead}>Marcar como leídas</button>
+            <h1>General</h1>
+            <p>Información general del usuario.</p>
           </div>
         )}
-
-        <form onSubmit={handleSubmit} className={styles.profileForm}>
-          <div style={{ position: 'relative' }}>
-            <Image
-              src={editableFields.avatarUrl}
-              alt="Avatar"
-              width={150}
-              height={150}
-              style={{ borderRadius: '50%' }}
-            />
-            {editableFields.notifications > 0 && (
-              <span className={styles.badge}>
-                {editableFields.notifications}
-              </span>
-            )}
+        {selectedSection === 'garage' && (
+          <div>
+            <h1>Garaje</h1>
+            <p>Aquí se muestran los autos del usuario.</p>
           </div>
-
-          <label>
-            Cambiar imagen de perfil:
-            <input type="file" onChange={handleImageChange} />
-          </label>
-
-          <label>
-            Nombre:
-            <input
-              type="text"
-              name="firstName"
-              value={editableFields.firstName}
-              onChange={handleInputChange}
-              className={styles.inputField} 
-            />
-          </label>
-
-          <label>
-            Apellidos:
-            <input
-              type="text"
-              name="lastName"
-              value={editableFields.lastName}
-              onChange={handleInputChange}
-              className={styles.inputField}
-            />
-          </label>
-
-          <label>
-            Email:
-            <input
-              type="email"
-              name="email"
-              value={editableFields.email}
-              onChange={handleInputChange}
-              className={styles.inputField}
-            />
-          </label>
-
-          <button type="submit">Guardar Cambios</button>
-        </form>
+        )}
+        {selectedSection === 'settings' && (
+          <div>
+            <h1>Configuración</h1>
+            <p>Opciones de configuración del usuario.</p>
+          </div>
+        )}
+        {selectedSection === 'adminPanel' && (
+          <div>
+            <h1>Panel de administrador</h1>
+            <p>Acceso a las funciones de administrador.</p>
+          </div>
+        )}
+        {selectedSection === 'requests' && (
+          <div>
+            <h1>Solicitudes</h1>
+            <p>Aquí se muestran las solicitudes pendientes.</p>
+          </div>
+        )}
       </div>
     </div>
   );
