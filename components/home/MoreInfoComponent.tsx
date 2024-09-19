5910413591333b78045e027c7b849ad6b7bbf2e6
Emilio R. Estévez González <eeg@pipelinepro.io>
2024-09-19 17:46:00 +0100

    Added React-FullPage & modified UI animations

--- components/home/MoreInfoComponent.tsx
+++ components/home/MoreInfoComponent.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+"use client"; // Asegura que el archivo se ejecute en el cliente
+
+import React, { useState } from 'react';
 import styles from '../../styles/home/MoreInfo.module.css';
+import LoginModal from '../auth/LoginModal';
+import SignUpModal from '../auth/SignUpModal';
 
 const Section3: React.FC = () => {
+  const [isSignUpModalOpen, setIsSignUpModalOpen] = useState(false);
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+
+  const openLoginModal = () => {
+    setIsLoginModalOpen(true);
+    setIsSignUpModalOpen(false);
+  };
+
+  const closeLoginModal = () => setIsLoginModalOpen(false);
+
+  const openSignUpModal = () => {
+    setIsSignUpModalOpen(true);
+    setIsLoginModalOpen(false);
+  };
+
+  const closeSignUpModal = () => setIsSignUpModalOpen(false);
+
   return (
     <section className={styles.section3}>
       <h2>Empresas Colaboradoras</h2>
@@ -12,10 +33,21 @@ const Section3: React.FC = () => {
       </div>
 
       {/* Botón para unirse */}
-      <button className={styles.joinButton}>Únete al Club</button>
+      <button className={styles.joinButton} onClick={openSignUpModal}>Únete al Club</button>
 
       {/* Indicador de apps futuras */}
       <p>Próximamente disponible en iOS y Android</p>
+
+      <LoginModal
+        isOpen={isLoginModalOpen}
+        onClose={closeLoginModal}
+        onSwitchToSignUp={openSignUpModal}
+      />
+      <SignUpModal
+        isOpen={isSignUpModalOpen}
+        onClose={closeSignUpModal}
+        onSwitchToLogin={openLoginModal}
+      />
     </section>
   );
 };
